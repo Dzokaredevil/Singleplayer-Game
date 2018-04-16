042c84d7a38ae3f7abe9dc604dd4b2a6513af8d8
Dzokaredevil <dima.voytovich@mail.ru>
2018-04-16 05:26:37 +0300

    task 21

--- js/Entities.js
+++ js/Entities.js
@@ -66,6 +66,8 @@ Player = function(){
 	var super_update = self.update;
 	self.update = function(){
 		super_update();
+		if(self.pressingRight || self.pressingLeft || self.pressingDown || self.pressingUp)
+			self.spriteAnimCounter += 0.2;
 		if(self.pressingMouseLeft)
 			self.performAttack();
 		if(self.pressingMouseRight)
@@ -118,6 +120,44 @@ Actor = function(type,id,x,y,width,height,img,hp,atkSpd){
 	self.attackCounter = 0;
 	self.aimAngle = 0;
 	
+	self.spriteAnimCounter = 0;
+	
+	self.draw = function(){
+		ctx.save();
+		var x = self.x - player.x;
+		var y = self.y - player.y;
+		
+		x += WIDTH/2;
+		y += HEIGHT/2;
+		
+		x -= self.width/2;
+		y -= self.height/2;
+		
+		var frameWidth = self.img.width/3;
+		var frameHeight = self.img.height/4;
+		
+		var aimAngle = self.aimAngle;
+		if(aimAngle < 0)
+			aimAngle = 360 + aimAngle;
+		
+		var directionMod = 3;	//draw right
+		if(aimAngle >= 45 && aimAngle < 135)	//down
+			directionMod = 2;
+		else if(aimAngle >= 135 && aimAngle < 225)	//left
+			directionMod = 1;
+		else if(aimAngle >= 225 && aimAngle < 315)	//up
+			directionMod = 0;
+		
+		var walkingMod = Math.floor(self.spriteAnimCounter) % 3;//1,2
+		
+		ctx.drawImage(self.img,
+			walkingMod*frameWidth,directionMod*frameHeight,frameWidth,frameHeight,
+			x,y,self.width,self.height
+		);
+		
+		ctx.restore();
+	}
+	
 	var super_update = self.update;
 	self.update = function(){
 		super_update();
@@ -163,6 +203,7 @@ Enemy = function(id,x,y,width,height,img,hp,atkSpd){
 	var super_update = self.update; 
 	self.update = function(){
 		super_update();
+		self.spriteAnimCounter += 0.2;
 		self.updateAim();
 		self.performAttack()
 	}
@@ -361,3 +402,4 @@ Bullet.generate = function(actor,aimOverwrite){
 	var spdY = Math.sin(angle/180*Math.PI)*5;
 	Bullet(id,x,y,spdX,spdY,width,height,actor.type);
 }
+
